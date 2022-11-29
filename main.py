9504f38ef9b27bf4b21bae49d987ab013306bd17
Jennifer J Lew Munoz <jenniferlewmunoz@gmail.com>
2022-11-28 23:30:45 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,47 @@
+def on_on_overlap(sprite, otherSprite):
+    Shark.say_text("\"Chomp chomp chomp\"", 500, False)
+    music.wawawawaa.play()
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap)
+
+Shark: Sprite = None
+scene.set_background_color(8)
+Fish = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . c c c c . . . . 
+            . . . . . . c c d d d d c . . . 
+            . . . . . c c c c c c d c . . . 
+            . . . . c c 4 4 4 4 d c c . . . 
+            . . . c 4 d 4 4 4 4 4 1 c . c c 
+            . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+            . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+            f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+            f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+            f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+            . f 4 4 4 4 1 c 4 f 4 d f f f f 
+            . . f f 4 d 4 4 f f 4 c f c . . 
+            . . . . f f 4 4 4 4 c d b c . . 
+            . . . . . . f f f f d d d c . . 
+            . . . . . . . . . . c c c . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(Fish)
+Shark = sprites.create(img("""
+        ...........fffffff...ccfff..........
+            ..........fbbbbbbbffcbbbbf..........
+            ..........fbb111bbbbbffbf...........
+            ..........fb11111ffbbbbff...........
+            ..........f1cccc1ffbbbbbcff.........
+            ..........ffc1c1c1bbcbcbcccf........
+            ...........fcc3331bbbcbcbcccf..ccccc
+            ............c333c1bbbcbcbccccfcddbbc
+            ............c333c1bbbbbbbcccccddbcc.
+            ............c333c11bbbbbccccccbbcc..
+            ...........cc331c11bbbbccccccfbccf..
+            ...........cc13c11cbbbcccccbbcfccf..
+            ...........c111111cbbbfdddddc.fbbcf.
+            ............cc1111fbdbbfdddc...fbbf.
+            ..............cccfffbdbbfcc.....fbbf
+            ....................fffff........fff
+    """),
+    SpriteKind.enemy)
+Shark.follow(Fish, 50)
---
+++ main.ts
@@ -0,0 +1,43 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Shark.sayText("\"Chomp chomp chomp\"", 500, false)
+})
+let Shark: Sprite = null
+scene.setBackgroundColor(8)
+let Fish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Fish)
+Shark = sprites.create(img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `, SpriteKind.Enemy)
+Shark.follow(Fish, 50)
